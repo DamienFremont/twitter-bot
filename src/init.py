b434b757d312815252760bcd11a57335f42cc425
Damien Frémont <damien.fremont@gmail.com>
2021-03-18 13:17:59 -0400

    fix import

--- src/init.py
+++ src/init.py
@@ -3,11 +3,7 @@ import logging
 import datetime
 import config
 from twitterbot.config import create_api
-from twitterbot.followfile import follow_file
 from twitterbot.followfile import init_files
-from twitterbot.tweetfile import tweet_random_file
-from twitterbot.tweetfile import tweet_file
-from twitterbot.tweetfile import open_folder
 import os
 
 logger = logging.getLogger('twitter')
@@ -24,7 +20,7 @@ def run():
     config.switch(account)
     api = create_api()
     # 
-    init_files(api, 'Cars2048', ['HSlaughterGame'])
+    init_files(api, 'MichTheGameDev', ['Cars2048'])
 
 def main():
     run()

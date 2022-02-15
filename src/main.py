2f625789e657c25edb26d32f7abd38387fe6e7ff
Damien Frémont <damien.fremont@gmail.com>
2022-02-14 23:22:54 -0800

    refactoring: distutils deprecated

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import sys, os, glob, getopt, time
-from distutils.util import strtobool
 import logging
 import config, log
 import twitterbot
@@ -158,6 +157,23 @@ def getargs(argv, configs, helpmsg=None):
             res[conf['longopt']] = conf['defarg']
         print(f"argument --{conf['longopt']}: '{res[conf['longopt']]}'")
     return res
+
+# DEPRECATED from distutils.util import strtobool
+# 'For these functions, and any others not mentioned here, you will need to reimplement the functionality yourself'
+def strtobool (val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
 
 # SCRIPT **********************************************************************
 
